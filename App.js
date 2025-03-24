44e13757d2bdb4fab2da7c01fee369f9a9232f62
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-03-24 16:28:23 -0600

    Add images

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AlertComponent from './component/Alert';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.text}>Hello word!</Text>
-			<StatusBar style='auto' />
+			<Text style={styles.text}>Hello word</Text>
+
+			<Image
+				source={{ uri: 'https://picsum.photos/200/300' }}
+				style={styles.image}
+			/>
+
+			<Button
+				title='Click me'
+				onPress={() => console.log('Button clicked')}
+			/>
 		</View>
 	);
 }
@@ -22,5 +29,29 @@ const styles = StyleSheet.create({
 	text: {
 		color: '#9cdcfe',
 		fontSize: 20,
+	},
+	image: {
+		width: 200,
+		height: 300,
+	},
+	batmanLogo: {
+		width: 200,
+		height: 100,
+		marginTop: 50,
+	},
+});
+
+const calculate = StyleSheet.create({
+	resultimc: {
+		marginTop: 50,
+		// background: url('/sites/all/statics/salud/imc/barraimc.png')no-repeat,
+		width: 534,
+		height: 101,
+		margin: 'auto',
+		textAlign: 'left',
+	},
+	resultimcImg: {
+		marginTop: -3,
+		marginLeft: 0,
 	},
 });
